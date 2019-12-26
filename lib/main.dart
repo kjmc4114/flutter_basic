6857da2b5dcaed9848a5a17945deb8c0981765d2
KONOS-PC1 <kjmc4114@naver.com>
2019-12-26 17:31:43 +0900

    flutter_start

--- lib/main.dart
+++ lib/main.dart
@@ -2,49 +2,54 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget { // 화면이 변경될 소지가 없음.
+class MyApp extends StatelessWidget {
+  // 화면이 변경될 소지가 없음.
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( // 안드로이드의 머터리얼 앱
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-          appBar: AppBar(
-            title:  Text('밤편지'),
-          ),
-          body: Text('???2', style: TextStyle(fontSize: 30))
-      )
-    );
+    return MaterialApp(
+        // 안드로이드의 머터리얼 앱
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HelloPage());
   }
 }
 
-class HelloPage extends StatefulWidget { // 변경사항이 따로없다.
-  final String title;
-  HelloPage(this.title);
+class HelloPage extends StatefulWidget {
+  // 변경사항이 따로없다.
   @override
   _HelloPageState createState() => _HelloPageState();
 }
 
 class _HelloPageState extends State<HelloPage> {
+  String _message = '안녕세상아2';
+  int _counter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add), onPressed: _changeMessage),
         appBar: AppBar(
-          title:  Text('밤편지'),
+          title: Text('밤편지'),
         ),
-        body: Text('HelloWorld2', style: TextStyle(fontSize: 30))
-    );
+        body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center, // 정렬방향을 아래로 및 센터
+                children: <Widget>[
+                  Text(_message, style: TextStyle(fontSize: 30)),
+                  Text('$_counter', style: TextStyle(fontSize: 30)), // _counter.toString = '$_count'
+                ],
+              )
+        ) // widget = HelloPage를 말함
+        );
+  }
+
+  void _changeMessage() {
+    setState(() {
+      _message = '헬로월드';
+      _counter++;
+    });
   }
 }
